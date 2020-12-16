923571e23742ca05878abe0235e022f76e03ff3e
Hyuk Son_RS <hs924_RS@ciserrsch.cornell.edu>
2020-12-15 23:23:01 -0500

    update codes and add additional stats

--- programs/05_jira_stats_graphs.R
+++ programs/05_jira_stats_graphs.R
@@ -61,6 +61,72 @@ stargazer(issues_total_journal,style = "aer",
           float = FALSE
 )
 
+#### Break out of the issues
+jira.issues.breakout <- jira.pyear %>%
+  arrange(desc(row_number())) %>%
+  group_by(ticket) %>%
+  mutate(status_order =  row_number(), st = "Status") %>%
+  mutate(new1 = paste(st, status_order, sep="")) %>%
+  select(ticket,Status,new1) %>%
+  pivot_wider(names_from = new1, values_from = "Status")
+
+ji <- jira.pyear %>%
+  select(ticket,Journal) %>%
+  distinct(ticket, .keep_all = TRUE) 
+
+jis <- jira.pyear %>%
+  filter(Status == "Submitted to MC" & Journal != "AEA P&P") %>%
+  select(ticket) %>%
+  distinct() %>%
+  mutate(submitted="Yes")
+
+jira.issues.breakout2 <- jira.issues.breakout %>%
+  left_join(jis,by="ticket") %>%
+  transform(submitted=ifelse(is.na(submitted),"No",as.character(submitted))) %>%
+  filter(submitted=="No") %>%
+  select(-Status26,-Status27,-Status28,-Status29,-Status30,-Status31,-Status32,-Status33) %>%
+  transform(final_status=ifelse(!is.na(Status25),as.character(Status25),
+                                ifelse(!is.na(Status24),as.character(Status24),
+                                       ifelse(!is.na(Status23),as.character(Status23),
+                                              ifelse(!is.na(Status22),as.character(Status22),
+                                                     ifelse(!is.na(Status21),as.character(Status21),
+                                                            ifelse(!is.na(Status20),as.character(Status20),
+                                                                   ifelse(!is.na(Status19),as.character(Status19),
+                                                                          ifelse(!is.na(Status18),as.character(Status18),
+                                                                                 ifelse(!is.na(Status17),as.character(Status17),
+                                                                                        ifelse(!is.na(Status16),as.character(Status16),
+                                                                                               ifelse(!is.na(Status15),as.character(Status15),
+                                                                                                      ifelse(!is.na(Status14),as.character(Status14),
+                                                                                                             ifelse(!is.na(Status13),as.character(Status13),
+                                                                                                                    ifelse(!is.na(Status12),as.character(Status12),
+                                                                                                                           ifelse(!is.na(Status11),as.character(Status11),
+                                                                                                                                  ifelse(!is.na(Status10),as.character(Status10),
+                                                                                                                                         ifelse(!is.na(Status9),as.character(Status9),
+                                                                                                                                                ifelse(!is.na(Status8),as.character(Status8),
+                                                                                                                                                       ifelse(!is.na(Status7),as.character(Status7),
+                                                                                                                                                              ifelse(!is.na(Status6),as.character(Status6),
+                                                                                                                                                                     ifelse(!is.na(Status5),as.character(Status5),
+                                                                                                                                                                            ifelse(!is.na(Status4),as.character(Status4),
+                                                                                                                                                                                   ifelse(!is.na(Status3),as.character(Status3),
+                                                                                                                                                                                          ifelse(!is.na(Status2),as.character(Status2),
+                                                                                                                                                                                                 ifelse(!is.na(Status1),as.character(Status1),"")))))))))))))))))))))))))) %>%
+  select(ticket,final_status)
+
+jira.issues.breakout <- jira.issues.breakout %>%  
+  left_join(jis,by="ticket") %>%
+  left_join(ji,by="ticket") %>%
+  transform(submitted=ifelse(is.na(submitted),"No",as.character(submitted))) %>%
+  select(ticket,Journal,submitted) %>%
+  left_join(jira.issues.breakout2, by="ticket") %>%
+  transform(outcome=ifelse(Journal=="AEA P&P","P&P",
+                           ifelse(final_status=="In Progress"|final_status=="Report Under Review"|final_status=="Write Preliminary Report"|final_status=="Verification"|final_status=="Pre-Approved"|final_status=="Approved"|final_status=="Data"|final_status=="Waiting for info"|final_status=="Waiting for external report","Not yet submitted","Others")))  %>%
+  transform(outcome=ifelse(submitted=="Yes","Submitted",as.character(outcome)))  %>%
+  group_by(outcome) %>%
+  summarise(n_outcome = n_distinct(ticket)) 
+
+
+   filter(outcome!="P&P", outcome!="Not yet submitted")
+
 
 #### Number of reports processed (went past submitted to MC) since December 1, 2019
 ## Total submitted records
