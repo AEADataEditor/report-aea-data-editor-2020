c4c8da2cff5c9f364fb513838ad4948b21f29c2b
Lars Vilhuber <lars@vilhuber.com>
2021-04-30 11:11:52 -0400

    Adding scholarone files to deposit

--- prepare_deposit.sh
+++ prepare_deposit.sh
@@ -3,5 +3,5 @@
 outfile=deposit-repository-$(date +%F).zip
 
 zip -rp $outfile README* tables/ programs/ images/
-zip -rp $outfile data/jira/anon  data/*txt 
+zip -rp $outfile data/jira/anon  data/*txt data/scholarone
 zip -rp $outfile $(find data -name README\*)
